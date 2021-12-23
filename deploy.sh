f42d149961d9f9f5e8ccd5dc8b85c83b7d851cbb
Alfonso Acosta <alfonso@stellar.org>
2021-12-23 18:03:22 +0100

    Fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ./cross-rpi.sh --features dbus_mpris --release 
 ssh rpi4 sudo supervisorctl stop spotifyd
-scp  rpi4:
-ssh rpi sudo supervisorctl start spotifyd
+scp target/armv7-unknown-linux-gnueabihf/release/spotifyd rpi4:
+ssh rpi4 sudo supervisorctl start spotifyd
 
-ssh rpi tail -f /var/log/spotifyd.log
+ssh rpi4 tail -f /var/log/spotifyd.log
