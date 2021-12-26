b257f4721d41082801ff5f35d256cc3b0e4cabe8
Alfonso Acosta <alfonso@stellar.org>
2021-12-26 02:30:58 +0100

    Use systemd to start spotifyd since supervisor starts it before dbus is ready
    Spotifyd should retry indefinitetly until it registers the service
    (and should try to re-register when it fails in case the Bus is restarted).
    But ... life

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./cross-rpi.sh --features dbus_mpris --release 
-ssh rpi4 sudo supervisorctl stop spotifyd
-scp target/armv7-unknown-linux-gnueabihf/release/spotifyd rpi4:
-ssh rpi4 sudo supervisorctl start spotifyd
+ssh duchamp sudo systemctl stop spotifyd
+scp duchamp/armv7-unknown-linux-gnueabihf/release/spotifyd duchamp:
+ssh duchamp sudo systemctl start spotifyd
 
-ssh rpi4 tail -f /var/log/spotifyd.log
+ssh duchamp journalctl -u spotifyd.service -f
