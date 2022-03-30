141f1aee1029c68043252788a38ec28ce06a26fd
Alfonso Acosta <alfonso@stellar.org>
2022-03-30 20:20:16 +0200

    Fix deploy path

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ./cross-rpi.sh --features dbus_mpris --release 
 ssh duchamp sudo systemctl stop spotifyd
-scp duchamp/armv7-unknown-linux-gnueabihf/release/spotifyd duchamp:
+scp target/armv7-unknown-linux-gnueabihf/release/spotifyd duchamp:
 ssh duchamp sudo systemctl start spotifyd
 
 ssh duchamp journalctl -u spotifyd.service -f
